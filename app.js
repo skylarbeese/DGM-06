84465205caf5a297cbaad7c855bda95f8fa57248
SKYLAR BEESE <skylarbeese@gmail.com>
2021-03-26 12:53:37 -0600

    commiy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const Verbs = verbs.split(" ", 3);
 console.log(Nouns)
  console.log(Ad)
  console.log(Verbs)
-    const MyStory = `once apon a time there was a ${Nouns[0]} that lived in a ${ad[0]}, ${Nouns[1]}. the ${Nouns[0]},
+    const MyStory = `once apon a time there was a ${Nouns[0]} that lived in a ${Ad[0]}, ${Nouns[1]}. the ${Nouns[0]},
     liked to ${Verbs[0]} in the big ${Nouns[2]}. one day he meet his friend ${Nouns[3]} and they both went ${Verbs[0]}
     together after they ${Verbs[2]} in the ${Ad[1]}, ${Nouns[2]}. they both lived ${Ad[2]} ever after as they ${Verbs[1]} in the ${Nouns[1]}.
 
