9b64b42dae584c2dfd74dda0e44b219466f49e9e
SKYLAR BEESE <skylarbeese@gmail.com>
2021-04-23 19:09:31 -0600

    th

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const TellMyStory = document.querySelector(".story")
 
 document.querySelector(".btn").onclick = () => {
 //function tellStory() {
-//const Nouns = document.querySelector(".nouns").value.toLowerCase().split(" ");
+
 const nouns = document.querySelector(".nouns").value.toLowerCase()
 
 let news1 = new String(nouns)
@@ -13,11 +13,11 @@ const Nouns = news1.split(' ');
 const ad = document.querySelector(".ad").value.toLowerCase()
 
 let news2 = new String(ad)
-const Ad = news2.split(' ', 3);
+const Ad = news2.split(' ');
 
 const verbs = document.querySelector(".verbs").value.toLowerCase()
 let news3 = new String(verbs)
-const Verbs = news3.split(' ', 3);
+const Verbs = news3.split(' ');
 
 //console.log(nouns)
 //const Nouns = nouns.split(" ", 3);
@@ -32,7 +32,7 @@ console.log(news3)
 
  console.log(Nouns)
  console.log(Verbs)
- //console.log(Verbs)
+
     const MyStory = `once apon a time there was a ${Nouns[0]} that lived in a ${Ad[0]}, ${Nouns[1]}. the ${Nouns[0]},
     liked to ${Verbs[0]} in the big ${Nouns[2]}. one day he meet his friend ${Nouns[2]} and they both went ${Verbs[0]}
     together after they ${Verbs[2]} in the ${Ad[1]}, ${Nouns[2]}. they both lived ${Ad[2]} ever after as they ${Verbs[1]} in the ${Nouns[1]}.`
